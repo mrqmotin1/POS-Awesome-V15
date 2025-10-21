f45379febc12d842f13ff6e326a3f850e1790a01
Abu Bakar <abubakar@Abus-MacBook-Air.local>
2025-10-21 16:11:20 +0600

    populate barcode field on sale invoice item

--- frontend/src/posapp/components/pos/ItemsSelector.vue
+++ frontend/src/posapp/components/pos/ItemsSelector.vue
@@ -1897,6 +1897,7 @@ export default {
 				new_item.item_barcode.forEach((element) => {
 					if (search === element.barcode) {
 						new_item.uom = element.posa_uom;
+						new_item.barcode = this.search;
 						match = true;
 					}
 				});
