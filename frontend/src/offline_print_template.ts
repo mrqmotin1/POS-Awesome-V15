ba54d559ed72a7c90fddfcd7a5610a4ced3c8bbb
Abu Bakar <abubakar@Abus-MacBook-Air.local>
2025-10-31 16:34:27 +0600

    update offline print format

--- frontend/src/offline_print_template.ts
+++ frontend/src/offline_print_template.ts
@@ -186,9 +186,12 @@ function defaultOfflineHTML(invoice) {
     // Format items into table rows.
     const itemsRows = (invoice.items || [])
         .map(
-            (item) => `
+            (item, index) => `
                 <tr>
-                    <td>${item.item_name || ""}</td>
+                    <td style="text-align: left;">${index + 1}</td>
+                    <td>${item.item_name || ""}
+                        <div>(${ item.barcode ? item.barcode : item.item_barcode[0].barcode})</div>
+                    </td>
                     <td style="text-align: center;">${item.qty || 0}</td>
                     <td style="text-align: center;">${(item.rate || 0)}</td>
                     <td style="text-align: right;">${(item.amount || 0).toFixed(2)}</td>
@@ -285,10 +288,9 @@ function formatDate(dateString) {
                 <div class="receipt">
                     <div style="text-align: center;">
                         <strong>${invoice.company || ""}</strong><br>
-                        ${invoice.company.registration_details || ""}<br>
-                        Ph: ${invoice.company.phone_no || ""}<br>
-                        ${invoice.company.website || ""}<br>
-                        TRN: ${invoice.company.tax_id || "N/A"}<br>
+                        ${company.registration_details || ""}<br>
+                        Tel: ${company.phone_no || ""}<br>
+                        TRN: ${company.tax_id || "N/A"}<br>
                     </div>
 
                     <hr style="border-top: 1px dashed #000; margin: 6px 0;">
@@ -316,6 +318,7 @@ function formatDate(dateString) {
                     <table width="100%" style="font-size: 11px;">
                         <thead>
                             <tr>
+                                <th style="text-align: left;">SL</th>
                                 <th style="text-align: left;">Items</th>
                                 <th style="text-align: center;">Qty.</th>
                                 <th style="text-align: center;">Price</th>
@@ -379,7 +382,7 @@ function formatDate(dateString) {
                         Exchange within 7 days with original bill and original packing. No exchange on under garments.
                     </p>
                     <p style="text-align: center; margin-top: 6px; font-size: 12px;">
-                        Thank you. Come again. / شكراً لزيارتكم 
+                        Thank you. Come again.
                     </p> 
                 </div>
             </body>
