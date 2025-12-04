cc4ef1f62156f68a2bde88bbcd8fad241071c047
Abu Bakar <abubakar@Abus-MacBook-Air.local>
2025-12-04 20:59:00 +0600

    fix overrride issue

--- posawesome/posawesome/api/invoices.py
+++ posawesome/posawesome/api/invoices.py
@@ -448,7 +448,7 @@ def update_invoice(data):
     invoice_doc.set_missing_values()
 
     # Reapply any custom item names after defaults are set
-    _apply_item_name_overrides(invoice_doc, overrides)
+    # _apply_item_name_overrides(invoice_doc, overrides)
 
     # Remove duplicate taxes from item and profile templates
     _merge_duplicate_taxes(invoice_doc)
@@ -781,7 +781,7 @@ def submit_invoice(invoice, data):
             )
 
     # Ensure item name overrides are respected on submit
-    _apply_item_name_overrides(invoice_doc)
+    # _apply_item_name_overrides(invoice_doc)
     if invoice.get("posa_delivery_date"):
         invoice_doc.update_stock = 0
     mop_cash_list = [
