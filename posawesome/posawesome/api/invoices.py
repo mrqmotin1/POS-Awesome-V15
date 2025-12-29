f30053becffd91f6e32d8d6db71136c628dbaced
Abu Bakar <abubakar@Abus-MacBook-Air.local>
2025-12-29 18:11:53 +0600

    fixed change amount

--- posawesome/posawesome/api/invoices.py
+++ posawesome/posawesome/api/invoices.py
@@ -982,7 +982,7 @@ def submit_invoice(invoice, data, submit_in_background=False):
         )
     else:
         invoice_doc.submit()
-        _create_change_payment_entries(invoice_doc, data, pos_profile, cash_account)
+        # _create_change_payment_entries(invoice_doc, data, pos_profile, cash_account)
         redeeming_customer_credit(invoice_doc, data, is_payment_entry, total_cash, cash_account, payments)
 
     return {"name": invoice_doc.name, "status": invoice_doc.docstatus}
@@ -1027,7 +1027,7 @@ def submit_in_background_job(kwargs):
     invoice_doc.save()
 
     invoice_doc.submit()
-    _create_change_payment_entries(invoice_doc, data, invoice_doc.pos_profile, cash_account)
+    # _create_change_payment_entries(invoice_doc, data, invoice_doc.pos_profile, cash_account)
     redeeming_customer_credit(invoice_doc, data, is_payment_entry, total_cash, cash_account, payments)
 
 
