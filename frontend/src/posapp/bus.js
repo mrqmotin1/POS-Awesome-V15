f319c4249a5d5ca0f97db450ed14deab9f0fe54a
Defendicon <robin001100110@gmail.com>
2026-02-01 23:22:46 +0500

    fix closing shift by resolving bus events

--- frontend/src/posapp/bus.js
+++ frontend/src/posapp/bus.js
@@ -2,8 +2,14 @@ import mitt from "mitt";
 
 export default {
 	install: (app, options) => {
+		const bus = mitt();
 		app.config.globalProperties.__ = window.__;
 		app.config.globalProperties.frappe = window.frappe;
-		app.config.globalProperties.eventBus = mitt();
+		app.config.globalProperties.eventBus = bus;
+
+		// Provide for Composition API usage
+		app.provide("eventBus", bus);
+		app.provide("__", window.__);
+		app.provide("frappe", window.frappe);
 	},
 };
